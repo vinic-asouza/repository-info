73dd54f12501c64695b211ec62543fb0bc67fe3e
Prisma MBP 103 <prisma@Assis.local>
2019-10-30 09:48:26 -0300

    feats: find repositories from github / axios / css animation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,28 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
-
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Container, Form, SubmitButton } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        Repositórios
-      </h1>
+import api from '../../services/api';
 
-      <Form onSubmit={() => { }}>
-        <input
-          type="text"
-          placeholder="Adicionar Repositório" />
+export default class Main extends Component {
 
-        <SubmitButton disabled>
-          <FaPlus color="#FFF" size={14} />
-        </SubmitButton>
-      </Form>
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  };
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true })
+
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
+  }
 
 
-    </Container>
-  );
+  render() {
+    const { newRepo, loading } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar Repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            {loading ? (<FaSpinner color="#FFF" size={14} />) : (<FaPlus color="#FFF" size={14} />)}
+          </SubmitButton>
+        </Form>
+
+
+      </Container>
+    );
+  }
 }
